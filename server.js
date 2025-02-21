ad05246d4a6d20f856b4cea5e1c88a35d0d42d29
Sanjay-adepu <adepusanjay812@gmail.com>
2025-02-21 23:27:22 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const ProjectRoutes = require("./routes/projectRoutes.js");
- 
+ const cors =require("cors");
  
 
 const app = express();
